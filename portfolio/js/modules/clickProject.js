87e47d5def4985373f9e83b3fd4b1cfd8ac4fdcf
julioeler <julioeler221@gmail.com>
2024-01-20 20:59:25 -0300

    otimizado js click projetos

--- portfolio/js/modules/clickProject.js
+++ portfolio/js/modules/clickProject.js
@@ -1,31 +1,35 @@
-export default function initClickProject() {}
-
-const projetos = document.querySelectorAll('.projetoDiv');
-projetos.forEach(projeto => {
+export default function initClickProject() {
+  const projetos = document.querySelectorAll('.projetoDiv');
+  projetos.forEach(projeto => {
     projeto.addEventListener('click', handleClick);
-});
-
-function handleClick() {
-  this.classList.add('ativo');
-  outsideClick(this, () =>{
-    this.classList.remove('ativo')
   });
-}
 
-function outsideClick(element, callback) {
-  const outside = 'data-ouside'
-  const html = document.documentElement;
+  function handleClick() {
+    this.classList.add('ativo');
+    outsideClick(this, ['touchstart', 'click'], () => {
+      this.classList.remove('ativo')
+    });
+  }
 
-  if(!element.hasAttribute(outside)){
-    html.addEventListener('click', handleOutsideClick);
-    element.setAttribute(outside, '');
-}
-  function handleOutsideClick(event) {
-    if(!element.contains(event.target)){
-      element.removeAttribute(outside);
-      html.removeEventListener('click', handleOutsideClick)
-      callback();
+  function outsideClick(element, events, callback) {
+    const html = document.documentElement;
+    const outside = 'data-ouside'
+
+    if (!element.hasAttribute(outside)) {
+      events.forEach(userEvent => {
+        html.addEventListener(userEvent, handleOutsideClick);
+      })
+      element.setAttribute(outside, '');
+    }
+
+    function handleOutsideClick(event) {
+      if (!element.contains(event.target)) {
+        element.removeAttribute(outside);
+        events.forEach(userEvent => {
+          html.removeEventListener(userEvent, handleOutsideClick)
+        })
+        callback();
+      }
     }
   }
 }
-
